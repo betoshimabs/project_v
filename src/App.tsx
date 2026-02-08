d433fa11d26fdf6a6eb3bdba6bff7c28667be359
betoshima <44249730+betoshima@users.noreply.github.com>
2026-02-08 00:21:20 -0300

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
